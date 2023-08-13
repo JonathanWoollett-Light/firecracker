5262168fb4f29c03d5c71b1d3fd7c422397955c5
Jonathan Woollett-Light <jcawl@amazon.co.uk>
2023-08-13 22:37:03 +0100

    feat: Replace `log` with `tracing`
    Replaces `log` with `tracing` and the custom logger with
    `tracing_subscriber::Fmt`.
    
    Adding support for re-configuring the logger at run-time and
    removing the old logger format.
    
    Signed-off-by: Jonathan Woollett-Light <jcawl@amazon.co.uk>

--- tests/integration_tests/build/test_binary_size.py
+++ tests/integration_tests/build/test_binary_size.py
@@ -14,11 +14,11 @@ MACHINE = platform.machine()
 
 SIZES_DICT = {
     "x86_64": {
-        "FC_BINARY_SIZE_TARGET": 3063120,
+        "FC_BINARY_SIZE_TARGET": 3428408,
         "JAILER_BINARY_SIZE_TARGET": 1080088,
     },
     "aarch64": {
-        "FC_BINARY_SIZE_TARGET": 2441008,
+        "FC_BINARY_SIZE_TARGET": 2928808,
         "JAILER_BINARY_SIZE_TARGET": 1040928,
     },
 }
